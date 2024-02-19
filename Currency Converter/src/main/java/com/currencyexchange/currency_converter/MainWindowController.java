40e3aea804767456d2e371fe658d36fdac82a1b5
Ayush Acharjya <ayushacharjya@gmail.com>
2024-02-19 14:25:32 +0000

    added db

--- Currency Converter/src/main/java/com/currencyexchange/currency_converter/MainWindowController.java
+++ Currency Converter/src/main/java/com/currencyexchange/currency_converter/MainWindowController.java
@@ -7,8 +7,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
--- Currency Converter/src/main/java/module-info.java
+++ Currency Converter/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.currencyexchange.currency_converter {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.json;
+    requires org.postgresql.jdbc;
 
 
     opens com.currencyexchange.currency_converter to javafx.fxml;
