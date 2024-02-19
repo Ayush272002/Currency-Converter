8e92da1ba14b199cd9933944da15ab4df0dd7b77
Ayush Acharjya <ayushacharjya@gmail.com>
2024-02-19 20:08:43 +0000

    final commit

--- Currency Converter/src/main/java/com/currencyexchange/currency_converter/Main.java
+++ Currency Converter/src/main/java/com/currencyexchange/currency_converter/Main.java
@@ -3,9 +3,12 @@ package com.currencyexchange.currency_converter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
@@ -13,6 +16,12 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-window-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 500);
         stage.setTitle("Currency Converter");
+
+        File file = new File("img/icon.png");
+        Image image = new Image(file.toURI().toString());
+        stage.getIcons().add(image);
+
+
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
