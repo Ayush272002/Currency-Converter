1357a4cc5832f462d447adcad2cbbcf00c02c30c
Ayush Acharjya <ayushacharjya@gmail.com>
2024-03-24 23:43:25 +0000

    change of module info

--- Currency Converter/src/main/java/module-info.java
+++ Currency Converter/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.currencyexchange.currency_converter {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.json;
-    requires org.postgresql.jdbc;
 
 
     opens com.currencyexchange.currency_converter to javafx.fxml;
